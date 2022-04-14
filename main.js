c570e6f6789f6683567cd445a77fff2a4f6282a4
bornin2k4 <84788741+bornin2k4@users.noreply.github.com>
2022-04-14 12:38:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log('IrrITable!');
+
+
+let agree = confirm('wanna close IrrITable???')
+
+
+while (true){
+    if (agree){
+      agree = confirm('wanna close IrrITable???')
+    }
+    else{
+      agree = confirm('wanna close IrrITable???')
+    }
+}
+
